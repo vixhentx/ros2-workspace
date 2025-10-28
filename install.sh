d660e6dfc9f38e9572aebf8b259034198ea29f77
vix_hentx <w1084349470@outlook.com>
2025-10-28 10:28:18 +0800

    自动化配置xhost,防止重启后失效 小改脚本,防止重复安装/卸载

--- install.sh
+++ install.sh
@@ -1,11 +1,41 @@
-#!bin/bash
-#用于安装rosup
-set -e
-INSTALL_DIR=~/.rosup
-cp -r . $INSTALL_DIR 
-chmod +x $INSTALL_DIR/rosup.sh
-echo "alias 'rosup=bash $INSTALL_DIR/rosup.sh'" >> ~/.bashrc
-source ~/.bashrc
-set -e
-echo "安装成功!"
-echo $INSTALL_DIR
+#!/bin/bash
+# rosup安装小脚本,可以自动配置rosup别名以及配置xhost
+set -euo pipefail
+
+INSTALL_DIR="$HOME/.rosup"
+XPROFILE_FILE="$HOME/.xprofile"
+BASHRC_FILE="$HOME/.bashrc"
+
+rm -rf "$INSTALL_DIR"
+cp -r . "$INSTALL_DIR"
+
+chmod +x "$INSTALL_DIR/rosup.sh"
+
+# xhost
+AUTOLOGIN_DIR=~/.config/autostart
+AUTOLOGIN_FILE=$AUTOLOGIN_DIR/docker-xhost.desktop
+
+mkdir -p $AUTOLOGIN_DIR
+
+cat > $AUTOLOGIN_FILE << EOL
+[Desktop Entry]
+Type=Application
+Name=Docker Xhost Fix
+Exec=sh -c "sleep 5 && xhost +local:docker"
+Icon=utilities-terminal
+Comment=Allow Docker containers to access the X server
+EOL
+
+chmod +x $AUTOLOGIN_FILE
+
+#rosup alias
+BASHRC_LINE="alias 'rosup=bash $INSTALL_DIR/rosup.sh'"
+touch "$BASHRC_FILE"
+if ! grep -qF -- "$BASHRC_LINE" "$BASHRC_FILE"; then
+  echo "$BASHRC_LINE" >> "$BASHRC_FILE"
+fi
+
+set -euo pipefail
+
+echo "rosup安装成功! 路径: $INSTALL_DIR"
+echo "要立即应用更改,请执行 . $BASHRC_FILE."
--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-#用于卸载rosup
-set -e
-INSTALL_DIR=~/.rosup
-rm -rf $INSTALL_DIR 
-sed -i "/alias rosup=/d" ~/.bashrc
-source ~/.bashrc
-set -e
-echo "卸载成功!"
-echo $INSTALL_DIR
+# 卸载 rosup
+
+set -euo pipefail
+
+INSTALL_DIR="$HOME/.rosup"
+XPROFILE_FILE="$HOME/.xprofile"
+BASHRC_FILE="$HOME/.bashrc"
+
+rm -rf "$INSTALL_DIR"
+
+#xhost
+AUTOLOGIN_DIR=~/.config/autostart
+AUTOLOGIN_FILE=$AUTOLOGIN_DIR/docker-xhost.desktop
+rm -f $AUTOLOGIN_FILE
+
+#rosup
+if [ -f "$BASHRC_FILE" ]; then
+  sed -i "/alias rosup=/d" "$BASHRC_FILE"
+fi
+
+set -euo pipefail
+
+echo "rosup 卸载成功!"
+echo "要立即应用更改,请执行 . $BASHRC_FILE."
